a0592d7eb3a1c16a61ad7a2fed1e12e4dcf182d9
Luis Zabala <luis.zabala076@gmail.com>
2019-02-20 18:17:38 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -60,7 +60,7 @@ end
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
   
-  def find_by_name
+  def find_by_name(name)
     sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
   DB[:conn].execute(sql)
   
