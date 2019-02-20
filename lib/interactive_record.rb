707111b57eb4551433ff9f0f99d8d3a33bab9ea0
Luis Zabala <luis.zabala076@gmail.com>
2019-02-20 18:24:24 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -11,6 +11,8 @@ class InteractiveRecord
   end
   
   def self.column_names
+    
+    
     sql = "pragma table_info('#{table_name}')"
     table_info = DB[:conn].execute(sql)
     column_names = []
