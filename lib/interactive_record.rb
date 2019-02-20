95bd8e6c7b1fbd3ea6531e4db8472732e5f1e8e8
Luis Zabala <luis.zabala076@gmail.com>
2019-02-20 15:47:04 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -12,6 +12,7 @@ class InteractiveRecord
     sql = "pragma table_info('#{table_name}')"
     table_info = DB[:conn].execute(sql)
     column_names = []
-    table_info.each {|row| column_names << row["name"]
+    table_info.each {|row| column_names << row["name"]}
 end
+
 end
