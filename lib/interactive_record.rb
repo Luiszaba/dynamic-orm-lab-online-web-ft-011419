f6308dd8c772fc12016aca02f1bfd1b82bb8edef
Luis Zabala <luis.zabala076@gmail.com>
2019-02-20 15:47:30 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -13,7 +13,6 @@ class InteractiveRecord
     table_info = DB[:conn].execute(sql)
     column_names = []
     table_info.each {|row| column_names << row["name"]}
-end
-column_names.compact
+    column_names.compact
 end
 end
