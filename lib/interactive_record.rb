8ee30d84de72cf83c4656b29b45a94fd76399ae4
Luis Zabala <luis.zabala076@gmail.com>
2019-02-20 18:28:00 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -25,35 +25,23 @@ class InteractiveRecord
   def initialize(options={})
     options.each do |property, value|
       self.send("#{property}=", value)
-      
-    # initialize the class with an empty hash.  This hash will have a key of properties and its values.  We can then iterate through the k,v with k being the properties so the hash  
   end
 end
 
   def table_name_for_insert
     self.class.table_name
-    
-    # this will become the basis for inserting a table name, by using the table_name method
   end
   
   def col_names_for_insert
     self.class.column_names.delete_if {|columns| columns == "id"}.join(", ")
-
-    # this method will be the basis for inserting column names into our db. The column will not be inserted if it already has an ID and deleted to prevent replications
   end
   
   def values_for_insert
     to_insert = []
        self.class.column_names.each do |column|
     to_insert << "'#{send(column)}'" unless send(column).nil?
-    
-    
-    # to set up our values for insertion, we need to first create where we will place our values.  I created an empty array called [to_insert].  Since we have our column_names method, lets take use of it.  We will iterate over the column_names and place those values in our new array.    We will NOT insert the column if its empty, this will create empty columns and we do not want that.
-    
     end
     to_insert.join(", ")
-    
-    # we need to remove the spacing, and none usable characters via .join(", ")  Now our array can be used without issues
   end
 
   def save 
